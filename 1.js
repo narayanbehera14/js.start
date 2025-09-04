84746f614930ab918a57a665a7888b59bb837606
lijubehera <beheranarayancs222330@gmail.com>
2025-09-04 14:20:28 +0530

    Initial commit

---
+++ 1.js
@@ -0,0 +1,31 @@
+console.log("hello");
+
+const student = {
+    name: 'raj',
+    age: '20',
+    class: '10'
+};
+console.log(student);
+
+const student2 = {
+    name: 'raj',
+    age: '20',
+    class: '10'
+};
+console.log(student2);
+
+const liju = {
+    hello: 'raj',
+    hi: 'no'
+};
+console.log(liju);
+
+var rohan = {
+    istrue: "Noo",
+    ok: "Ha",
+    omg: "Hee"
+};
+
+
+rohan["ok"] = rohan["ok"] + kk
+console.log(rohan);
