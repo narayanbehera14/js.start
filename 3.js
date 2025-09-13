8e7ba2ad0ed12fb1008727546b80fa93db800171
lijubehera <beheranarayancs222330@gmail.com>
2025-09-13 19:15:04 +0530

    Added new files

---
+++ 3.js
@@ -0,0 +1,14 @@
+// function greeting(parameterVariable) {
+//     console.log("Hello, World!");
+//     console.log(parameterVariable);
+// }
+
+// // Call the function and pass a string
+// greeting("Welcome to 10 Days of JavaScript!");
+
+
+function pass(hiii) {
+    console.log("hello");
+    console.log(hiii);
+}
+pass("omg") 
---
+++ 4.js
@@ -0,0 +1,26 @@
+// // // // let sum = 0;
+// // // // for(i = 0;i<=5;i++){
+// // // //     sum = sum + i;
+// // // //     console.log(sum)
+
+// // // // }
+// // // let sum = 0;
+// // // for ( let i = 1; i <= 5; i++) {
+// // //     sum = sum + i ; 
+
+// // // }
+// // // console.log(sum);
+// // // console.log("loop has end");
+
+
+// // for(let i = 0;i<=10;i++){
+// //     console.log(i)
+// // }
+
+// for(let i = 1;i <= 10;i+=2){
+//     console.log(i)
+// }
+
+for(let i = 1;i <= 10;i+1){
+    console.log(i)
+}
