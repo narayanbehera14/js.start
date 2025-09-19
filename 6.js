84ba63f11b5445a2ca131b6471e48e978c55f8a1
lijubehera <beheranarayancs222330@gmail.com>
2025-09-19 16:04:48 +0530

    Added 6.js file

---
+++ 6.js
@@ -0,0 +1,72 @@
+// // // // function  (myfunction){
+// // // //     console.log(hello narayan);
+// // // // //     console.log(hey liju);
+
+// // // // // }
+// // // // // myfunction()
+// // // // function myfunction(){
+// // // //     console.log("hey");
+// // // //     console.log("hiiiii");
+// // // // }
+// // // // myfunction()
+
+// // // function narayan(){
+// // //     console.log("behera");
+
+// // // }
+// // // narayan()
+// // function myfunction(x,y){
+// //     console.log(5+6);
+// // }
+// // myfunction();
+
+// function sum(x,y) {
+//     s = x+y;
+//     return s ;
+// }
+
+// let val = sum (3,5);
+// console.log(val);
+
+// function no(c,u){
+//     num = c-u;
+//     return num;
+// }
+// let val = no(9,2);
+// console.log(val)
+
+// const add = (x,y) => x-y;
+// // console.log(add(5,6));
+
+// // const val = (x,y) => x*y;
+// // console.log(val(8,9));
+
+// // function num(x,y){
+// //     val = x-y;
+// //     return val;
+
+// // }
+// // let nn = val(5,8)
+// // console.log(nn)
+
+// // function countVowels(str){
+// //     for(const char of str){
+// //         console.log(char);
+// //     }
+// // }
+
+// function countVowels(str){
+//     let count = 0;
+//     for (const char of str){
+//         if (
+//             char ==="a" ||
+//             char ==="e"||
+//             char ==="i"||
+//             char ==="o"||
+//             char ==="u"
+//         ) {
+//             count ++;
+//         }
+//     }
+//     console.log(count);
+// }
