8539b0f891eb4f5356f42d2dd80c7d31d7bebb04
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-10-25 17:36:06 +0530

    updated

---
+++ 12.js
@@ -0,0 +1,8 @@
+//classes & Objects
+const student = {
+    fullname : "narayan",
+    marks : 54,
+     printMarks : function () { // function are method
+        console.log("marks =",marks);
+     },
+};
